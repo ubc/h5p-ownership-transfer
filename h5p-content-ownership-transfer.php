f59cac4287a13ce7008bc970457fb124672cc636
kelvin <kelvin.xu@ubc.ca>
2022-02-03 12:37:12 -0800

    Update plugin version

--- h5p-content-ownership-transfer.php
+++ h5p-content-ownership-transfer.php
@@ -11,7 +11,7 @@
  * Plugin Name: UBC H5P Addon - Content Ownership Transfer
  * Plugin URI:  https://ubc.ca/
  * Description: Allowed users with certain capability to transfer the ownership of h5p content to another user.
- * Version:     1.0.5
+ * Version:     1.0.6
  * Author:      Kelvin Xu
  * Text Domain: ubc-h5p-addon-content-ownership-transfer
  * License:     GPL v2 or later
